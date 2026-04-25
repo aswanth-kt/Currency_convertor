aac5ef3f3b8c95bb45e981e8630d367d151b69e4
aswanthkt31 <aswanthkt99@gmail.com>
2026-04-26 00:22:36 +0530

    Refactor InputBox component to simplify currency select styling and remove unnecessary disabled state

--- src/components/inputes/InputBox.jsx
+++ src/components/inputes/InputBox.jsx
@@ -44,11 +44,9 @@ function InputBox({
         <p className="text-black/40 mb-2 w-full">Currency Type</p>
 
         <select 
-          className={`rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none
-            ${currencyDisabled ? 'cursor-not-allowed opacity-50 bg-gray-100' : ''}`}
+          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
           value={selectCurrency}
           onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
-          disabled={currencyDisabled}
         >
 
           {currencyOptions.map((currency) => (
