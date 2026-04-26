6db0ace84087d9098fd8c26641ac04959e27fc38
aswanthkt31 <aswanthkt99@gmail.com>
2026-04-26 12:43:19 +0530

    Remove minimum value attribute from amount input in InputBox component

--- src/components/inputes/InputBox.jsx
+++ src/components/inputes/InputBox.jsx
@@ -29,7 +29,6 @@ function InputBox({
         <input
           id={amountInputId}
           className={`outline-none w-full bg-transparent py-1.5 ${amountDisabled ? 'cursor-not-allowed' : ''}`}
-          min={0.00}
           type="number"
           placeholder="Amount"
           disabled={amountDisabled}
